ebb89976899a711c0cd818e0657b8ed2634f11d2
Maik <noungouamaik2023@outlook.com>
2024-03-09 10:30:44 +0000

    untracked chenges

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
 const cpuRecord = []; //this array append 1 for every round won by CPU 
 const playerRecord = []; //this array append 1 for every round won by player  
 const max = 3;
+let cpuScore = 0;
+let playerScore = 0;
 
 
 a = function getComputerChoice () {
@@ -26,14 +28,13 @@ a = function getComputerChoice () {
 
 
 function rpsRound (playerChoice, compChoice){
-    //compChoice = a();
-   // playerChoice = b();
 
     if (playerChoice === compChoice) {
         let result = "This is a draw";
         console.log(compChoice);
         console.log(playerChoice);
         alert(result);
+
     }
 
     else if (playerChoice == "Rock" && compChoice == "Paper" ){
@@ -41,18 +42,21 @@ function rpsRound (playerChoice, compChoice){
         console.log(compChoice);
         console.log(playerChoice);
         alert(result);
+        cpuScore = cpuScore + 1;
    }
     else if (playerChoice == "Scissors" && compChoice == "Rock" ){
         let result = "You Lose! Rock beats Scissors";
         console.log(compChoice);
         console.log(playerChoice);
         alert(result);
+        cpuScore = cpuScore + 1;
     }
     else if (playerChoice == "Paper" && compChoice == "Scissors"){
         let result = "You lose! Scissors beats Paper";
         console.log(compChoice);
         console.log(playerChoice);
         alert(result);
+        cpuScore = cpuScore + 1;
     }
 
     else {
@@ -60,41 +64,35 @@ function rpsRound (playerChoice, compChoice){
         console.log(compChoice);
         console.log(playerChoice);
         alert(result);
+        playerScore = playerScore + 1;
     }
 }
 
-function uiLogic (){
-    rock.addEventListener('click', () => {
-    playerChoice = "Rock";
-    rpsRound( playerChoice, compChoice);
-    });
 
-    paper.addEventListener('click', () => {
-    playerChoice = "Paper";
-    rpsRound(playerChoice, compChoice);
-    });
+/*rock.addEventListener('click', () => {
+playerChoice = "Rock";
+rpsRound( playerChoice, compChoice);
+});
 
-    scissors.addEventListener('click', () => {
-    playerChoice = "Scissors";
-    rpsRound(playerChoice, compChoice);
-    });
-}
+paper.addEventListener('click', () => {
+playerChoice = "Paper";
+rpsRound(playerChoice, compChoice);
+});
+
+scissors.addEventListener('click', () => {
+playerChoice = "Scissors";
+rpsRound(playerChoice, compChoice);
+});
+*/
 
 
 
 let compChoice = a(); //let's not forget to 
 start.addEventListener('click', () => {
     const container = document.getElementById("appender");
-    
 
     const playButton = document.getElementById("start");
     playButton.style.display = "none";
-
-    
-   /* const hiddenElements = document.querySelectorAll("hidden"); 
-    hiddenElements.style.display = "block"; 
-    /*const hiddenElements = document.getElementsByClassName("hidden"); 
-    hiddenElements.style.display = "block"; */
 
     const roundDiv = document.createElement('div');
     const roundIndicator = document.createElement('p');
@@ -106,8 +104,8 @@ start.addEventListener('click', () => {
 
     roundIndicator.textContent = `Round : ${"round"}` //The variable round needs 
     
-    playerChoicesIndicator.textContent = `Player 1 choses ${"playerChoice"}`;
-    cpuChoicesIndicator.textContent = `CPU choses ${"compChoice"}`;
+   // playerChoicesIndicator.textContent = `Player 1 choses ${"playerChoice"}`;
+  //  cpuChoicesIndicator.textContent = `CPU choses ${"compChoice"}`;
 
     container.appendChild(roundDiv);
     roundDiv.appendChild(roundIndicator);
@@ -115,17 +113,44 @@ start.addEventListener('click', () => {
     choicesDiv.appendChild(playerChoicesIndicator);
     choicesDiv.appendChild(cpuChoicesIndicator);
 
+    //for loop that will implement the game logic
 
+    for (i=1; i<2; i++){
+        console.log(i);
+        let playerScore = 0;
+        let cpuScore = 0;
+        let round = i;
+        roundIndicator.textContent = `Round : ${round}`;
 
+        if (){}
+        
+        rock.addEventListener('click', () => {
+            playerChoice = "Rock";
+            rpsRound( playerChoice, compChoice);
+            playerChoicesIndicator.textContent = `Player 1 choses ${"playerChoice"}`;
+            cpuChoicesIndicator.textContent = `CPU choses ${"compChoice"}`;
+            });
+        
+        paper.addEventListener('click', () => {
+            playerChoice = "Paper";
+            rpsRound(playerChoice, compChoice);
+            playerChoicesIndicator.textContent = `Player 1 choses ${"playerChoice"}`;
+            cpuChoicesIndicator.textContent = `CPU choses ${"compChoice"}`;
+            });
 
-     
+        scissors.addEventListener('click', () => {
+            playerChoice = "Scissors";
+            playerChoicesIndicator.textContent = `Player 1 choses ${playerChoice}`;
+            cpuChoicesIndicator.textContent = `CPU choses ${compChoice}`;
+            rpsRound(playerChoice, compChoice);
+            
+            });
 
+        //playerChoicesIndicator.textContent = `Player 1 choses ${playerChoice}`;
+       // cpuChoicesIndicator.textContent = `CPU choses ${compChoice}`;
+        
+    }
 
-
-
-
-
-//compChoice;
 
 });
 
